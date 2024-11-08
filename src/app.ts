403e5e0a5de14aabf7d885560ab96a88a98ea6b7
mohammad-r-sh <ms9647890@gmail.com>
2024-11-08 14:58:24 -0800

    main file updated

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import { config } from "dotenv";
+import { Request, Response, NextFunction } from "express";
+
+import AppError from "./utiles/AppError";
 
 config({ path: "./config.env" });
 
 const app = express();
+app.use(express.json());
 
-app.use("/", (_req, res) => {
-  res.end("Hello from server part");
+//endpoints
+app.use("/api/v1");
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 export default app;
