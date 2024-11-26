f114b27b9921d7529ea2e5f44398b8546573fd5d
mohammad-r-sh <ms9647890@gmail.com>
2024-11-26 05:20:26 -0800

    user router compelete

--- src/Routes/userRouter.ts
+++ src/Routes/userRouter.ts
@@ -2,13 +2,24 @@ import express from "express";
 import {
   userSignUp,
   login,
+  logout,
   protect,
   restrictTo,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
 } from "../Controllers/authController";
 
 const router = express.Router();
 
+// مسیرهای مربوط به احراز هویت
 router.route("/signup").post(protect, restrictTo("admin"), userSignUp);
 router.route("/login").post(login);
+router.route("/logout").get(logout);
+
+// مسیرهای مربوط به مدیریت رمز عبور
+router.route("/forgotPassword").post(forgotPassword);
+router.route("/resetPassword/:token").patch(resetPassword);
+router.route("/updatePassword").patch(protect, updatePassword);
 
 export default router;
