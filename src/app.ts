74f350d89a481d07b8432922d7dac236a020159b
mohammad-r-sh <ms9647890@gmail.com>
2024-11-26 05:21:05 -0800

    main app file update

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import AppError from "./utiles/AppError";
 
 import userRouter from "./Routes/userRouter";
+import classRouter from "./Routes/classRouter";
 
 config({ path: "./config.env" });
 
@@ -12,8 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-//endpoints
+// مسیرهای مربوط به کاربران
 app.use("/api/v1/users", userRouter);
+
+// مسیرهای مربوط به کلاس‌ها
+app.use("/api/v1/classes", classRouter);
 
 app.all("*", (req: Request, res: Response, next: NextFunction) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
